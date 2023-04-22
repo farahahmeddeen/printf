fa2970f521ac298e6d8237804b0791a5e2356446
Zablon <zablonkiprotich16@gmail.com.com>
2023-04-22 17:30:37 +0300

    Add _string.c file

--- _string.c
+++ _string.c
@@ -15,16 +15,28 @@ int _string(va_list zab)
 	ptr = va_arg(zab, char *);
 
 	if (ptr == NULL)
-		ptr = "NULL";
-
-	for (z = 0; ptr[z] != '\0'; z++)
-		;
-
-	f = 0;
-	while (f < z)
 	{
-		_putchar(ptr[f]);
-		f++;
+		ptr = "NULL";
+		for (z = 0; ptr[z] != '\0'; z++)
+			;
+		f = 0;
+		while (f < z)
+		{
+			_putchar(ptr[f]);
+			f++;
+		}
+		return (z);
 	}
-	return (z);
+	else
+	{
+		for (z = 0; ptr[z] != '\0'; z++)
+			;
+		f = 0;
+		while (f < z)
+		{
+			_putchar(ptr[f]);
+			f++;
+		}
+		return (z);
+	}
 }
