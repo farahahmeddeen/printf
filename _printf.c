eed3b3d27f88b5ee7f019e12407d059d7b0155a9
farahahmeddeen <farahahmeddeen1995@gmail.com>
2023-04-22 04:51:02 -0700

    Add _printf file

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _printf - function to print
+ * @format: what to print
+ * Return: the string to be printed
+ */
+int _printf(const char *format, ...)
+{
+	conversion_array fa[] = {
+		{"%c", _char},
+		{"%s", _string},
+		{"%%", _percent}
+	};
+	va_list zab;
+	int f, z, l;
+
+	va_start(zab, format);
+	if (format == NULL || (format[0] == "%" && format[1] == '\0'))
+		return (-1);
+	f = 0;
+	while (format[f] != '\0')
+	{
+		for (z = 0; z < 3; z++)
+		{
+			if (fa[z].h[0] == format[f] && fa[z].h[1] == format[f + 1])
+			{
+				l += fa[z].func(zab);
+				f = f + 2;
+				break;
+			}
+		}
+		_putchar(format[f]);
+		l++;
+		f++;
+	}
+	va_end(zab);
+	return (l);
+}
